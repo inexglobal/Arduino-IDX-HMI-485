10fc3a688fbd064a14299b4a6e11c04c9e13fc20
Krittamet Thawong <37761388+timor2542@users.noreply.github.com>
2025-11-12 12:03:51 +0700

    Update a mirobot header and cpp file

--- libraries/Mirobot/Mirobot.cpp
+++ libraries/Mirobot/Mirobot.cpp
@@ -3,228 +3,329 @@
 class HardwareSerial;
 HardwareSerial *serial;
 
-UART_Mirobot::UART_Mirobot(){
+UART_Mirobot::UART_Mirobot()
+{
 }
 
-void UART_Mirobot::serialInit(int serialPort){
+void UART_Mirobot::serialInit(int serialPort)
+{
   commMode = 0;
   mirobotSerialPort = serialPort;
- /* switch(mirobotSerialPort){
-    case 0: serial = &Serial; break;
-    case 1: serial = &Serial1; break;
-    case 2: serial = &Serial2; break;
-   // case 3: serial = &Serial3; break;
-    default:serial = &Serial; break;
+  switch (mirobotSerialPort)
+  {
+  case 0:
+    serial = &Serial;
+    break;
+  case 1:
+    serial = &Serial2;
+    break;
+  case 2:
+    serial = &Serial2;
+    break; // Note: case 2 also maps to Serial2
+           // case 3: serial = &Serial3; break;
+  default:
+    serial = &Serial;
+    break;
   }
-  */
-  serial = &Serial1;
-  serial->begin(115200);
+  serial->begin(38400);
 }
 
-void UART_Mirobot::setSerialMonitoring(bool mode){
+void UART_Mirobot::setSerialMonitoring(bool mode)
+{
   serialMonitoringMode = mode;
 }
 
-void UART_Mirobot::homing(int homingMode){
+void UART_Mirobot::homing(int homingMode)
+{
   setCmdTimeout(30000);
-  switch(homingMode){
-    case 1: sendMsg("$H1\nF2000"); break;
-    case 2: sendMsg("$H2\nF2000"); break;
-    case 3: sendMsg("$H3\nF2000"); break;
-    case 4: sendMsg("$H4\nF2000"); break;
-    case 5: sendMsg("$H5\nF2000"); break;
-    case 6: sendMsg("$H6\nF2000"); break;
-    case 7: sendMsg("$H7\nF2000"); break;
-    default: sendMsg("$H\nF2000"); break;
+  switch (homingMode)
+  {
+  case 1:
+    sendMsg("$H1\nF2000");
+    break;
+  case 2:
+    sendMsg("$H2\nF2000");
+    break;
+  case 3:
+    sendMsg("$H3\nF2000");
+    break;
+  case 4:
+    sendMsg("$H4\nF2000");
+    break;
+  case 5:
+    sendMsg("$H5\nF2000");
+    break;
+  case 6:
+    sendMsg("$H6\nF2000");
+    break;
+  case 7:
+    sendMsg("$H7\nF2000");
+    break;
+  default:
+    sendMsg("$H\nF2000");
+    break;
   }
   setCmdTimeout(10000);
 }
 
-void UART_Mirobot::setMoveSpeed(int velocity){
+void UART_Mirobot::setMoveSpeed(int velocity)
+{
   mirobotVelocity = velocity;
   sendMsg("F" + String(velocity));
 }
 
-void UART_Mirobot::setJumpPara(int height){
+void UART_Mirobot::setJumpPara(int height)
+{
   String strCmd = "$49=" + String(height);
   sendMsg(strCmd);
 }
 
-void UART_Mirobot::timedPause(float seconds){
+void UART_Mirobot::timedPause(float seconds)
+{
   String strCmd = "G04 P";
-  String strPara = String(seconds,3);
+  String strPara = String(seconds, 3);
   sendMsg(strCmd + strPara);
 }
 
-void UART_Mirobot::zero(){
-//  sendMsg("$M");
+void UART_Mirobot::zero()
+{
+  //  sendMsg("$M");
   sendMsg("M21 G90 G01 X0 Y0 Z0 A0 B0 C0");
 }
 
-void UART_Mirobot::moveJoints(float j1, float j2, float j3, float j4, float j5, float j6, bool motionMode){
+void UART_Mirobot::moveJoints(float j1, float j2, float j3, float j4, float j5, float j6, bool motionMode)
+{
   String strCmd = "M21 G00 ";
-  if(motionMode) strCmd.concat("G90 ");
-  else strCmd.concat("G91 ");
-  String strParam = "X"+String(j1, 2)+" Y"+String(j2, 2)+" Z"+String(j3, 2)+" A"+String(j4, 2)+" B"+String(j5, 2)+" C"+String(j6, 2);
+  if (motionMode)
+    strCmd.concat("G90 ");
+  else
+    strCmd.concat("G91 ");
+  String strParam = "X" + String(j1, 2) + " Y" + String(j2, 2) + " Z" + String(j3, 2) + " A" + String(j4, 2) + " B" + String(j5, 2) + " C" + String(j6, 2);
   strCmd.concat(strParam);
   sendMsg(strCmd);
 }
 
-void UART_Mirobot::movePose(float x, float y, float z, float rx, float ry, float rz, bool motionMode, int pathMode){
+void UART_Mirobot::movePose(float x, float y, float z, float rx, float ry, float rz, bool motionMode, int pathMode)
+{
   String strCmd = "M20 ";
-  switch(pathMode){
-    case MOVJ: strCmd.concat("G00 "); break;
-    case MOVL: strCmd.concat("G01 "); break;
-    case JUMP: strCmd.concat("G05 "); break;
-    default: strCmd.concat("G00 "); break;
+  switch (pathMode)
+  {
+  case MOVJ:
+    strCmd.concat("G00 ");
+    break;
+  case MOVL:
+    strCmd.concat("G01 ");
+    break;
+  case JUMP:
+    strCmd.concat("G05 ");
+    break;
+  default:
+    strCmd.concat("G00 ");
+    break;
   }
-  if(motionMode) strCmd.concat("G90 ");
-  else strCmd.concat("G91 ");
-  String strParam = "X"+String(x, 2)+" Y"+String(y, 2)+" Z"+String(z, 2)+" A"+String(rx, 2)+" B"+String(ry, 2)+" C"+String(rz, 2);
+  if (motionMode)
+    strCmd.concat("G90 ");
+  else
+    strCmd.concat("G91 ");
+  String strParam = "X" + String(x, 2) + " Y" + String(y, 2) + " Z" + String(z, 2) + " A" + String(rx, 2) + " B" + String(ry, 2) + " C" + String(rz, 2);
   strCmd.concat(strParam);
   sendMsg(strCmd);
 }
 
-void UART_Mirobot::moveArc(float x, float y, float z, float r, bool motionMode, int pathMode){
+void UART_Mirobot::moveArc(float x, float y, float z, float r, bool motionMode, int pathMode)
+{
   String strCmd = "M20 ";
-  if(motionMode) strCmd.concat("G90 ");
-  else strCmd.concat("G91 ");
-  if(pathMode) strCmd.concat("G02 ");
-  else strCmd.concat("G03 ");
-  String strParam = "X"+String(x, 2)+" Y"+String(y, 2)+" Z"+String(z, 2)+" R"+String(r, 2);
+  if (motionMode)
+    strCmd.concat("G90 ");
+  else
+    strCmd.concat("G91 ");
+  if (pathMode)
+    strCmd.concat("G02 ");
+  else
+    strCmd.concat("G03 ");
+  String strParam = "X" + String(x, 2) + " Y" + String(y, 2) + " Z" + String(z, 2) + " R" + String(r, 2);
   strCmd.concat(strParam);
   sendMsg(strCmd);
 }
 
-void UART_Mirobot::gripper(int gripperStatus){
-  if(gripperStatus) sendMsg("M3 S40");
-  else sendMsg("M3 S60");
+void UART_Mirobot::gripper(int gripperStatus)
+{
+  if (gripperStatus)
+    sendMsg("M3 S40");
+  else
+    sendMsg("M3 S60");
 }
 
-void UART_Mirobot::softGripper(int softGripperStatus){
-  if(softGripperStatus == ON) sendMsg("M3 S1000");
-  else if(softGripperStatus == GRAB) sendMsg("M3 S500");
-  else sendMsg("M3 S0");
+void UART_Mirobot::softGripper(int softGripperStatus)
+{
+  if (softGripperStatus == ON)
+    sendMsg("M3 S1000");
+  else if (softGripperStatus == GRAB)
+    sendMsg("M3 S500");
+  else
+    sendMsg("M3 S0");
 }
 
-void UART_Mirobot::pump(int pumpStatus){
-  if(pumpStatus == ON) sendMsg("M3 S1000");
-  else if(pumpStatus == BLOW) sendMsg("M3 S500");
-  else sendMsg("M3 S0");
+void UART_Mirobot::pump(int pumpStatus)
+{
+  if (pumpStatus == ON)
+    sendMsg("M3 S1000");
+  else if (pumpStatus == BLOW)
+    sendMsg("M3 S500");
+  else
+    sendMsg("M3 S0");
 }
 
-void UART_Mirobot::pwmWrite(int pwm){
+void UART_Mirobot::pwmWrite(int pwm)
+{
   String strPara = "S" + String(pwm);
   sendMsg("M3 " + strPara);
 }
 
-void UART_Mirobot::moveConveyor(float d, bool motionMode){
+void UART_Mirobot::moveConveyor(float d, bool motionMode)
+{
   sendMsg("$45=0");
   String strCmd = "G01 ";
-  if(motionMode) strCmd.concat("G90 ");
-  else strCmd.concat("G91 ");
+  if (motionMode)
+    strCmd.concat("G90 ");
+  else
+    strCmd.concat("G91 ");
   String strPara = "D " + String(d, 2);
   sendMsg(strCmd + strPara);
 }
 
-void UART_Mirobot::moveRail(float d, bool motionMode){
+void UART_Mirobot::moveRail(float d, bool motionMode)
+{
   sendMsg("$45=1");
   String strCmd = "G01 ";
-  if(motionMode) strCmd.concat("G90 ");
-  else strCmd.concat("G91 ");
+  if (motionMode)
+    strCmd.concat("G90 ");
+  else
+    strCmd.concat("G91 ");
   String strPara = "D " + String(d, 2);
   sendMsg(strCmd + strPara);
 }
 
-void UART_Mirobot::setCmdTimeout(long cmdTimeout){
+void UART_Mirobot::setCmdTimeout(long cmdTimeout)
+{
   mirobotCmdTimeout = cmdTimeout;
 }
 
-void UART_Mirobot::sendMsg(String gcode, bool check){
-  if(commMode == 0){
-	while(serial->read() >= 0){};
-    if(serial == reinterpret_cast<HardwareSerial*>(&Serial)||serialMonitoringMode == 0) {
-    serial->println(gcode);
+void UART_Mirobot::sendMsg(String gcode, bool check)
+{
+  if (commMode == 0)
+  {
+    while (serial->read() >= 0)
+    {
+    };
+    if (serial == reinterpret_cast<HardwareSerial *>(&Serial) || serialMonitoringMode == 0)
+    {
+      serial->println(gcode);
     }
-    else{
+    else
+    {
       serial->println(gcode);
       Serial.println("Send message: " + gcode);
     }
     unsigned long rtime = millis();
     strMsg = "";
-    while(strMsg.indexOf("ok")<0&&strMsg.indexOf("State")<0&&check&&strMsg.length()!=2){
-      //如果串口中有数据则全部读取
-      //if(serial->available()) strMsg += serial->readString();
-      if(serial->available()) strMsg += serial->readStringUntil('/r/n');
-      //如果等待结束符超时则跳出
-      if((millis()-rtime)>=(mirobotCmdTimeout)){
+    while (strMsg.indexOf("ok") < 0 && strMsg.indexOf("State") < 0 && check && strMsg.length() != 2)
+    {
+      // If there is data in the serial port, read all of it
+      // if(serial->available()) strMsg += serial->readString();
+      if (serial->available())
+        strMsg += serial->readStringUntil('/r/n'); // Note: This might be a typo for '\r' or '\n'
+      // If waiting for the end character times out, break out
+      if ((millis() - rtime) >= (mirobotCmdTimeout))
+      {
         strMsg += "---Timeout---";
         break;
       }
       delay(100);
     }
-    if(serial != reinterpret_cast<HardwareSerial*>(&Serial)&&serialMonitoringMode == 1){
+    if (serial != reinterpret_cast<HardwareSerial *>(&Serial) && serialMonitoringMode == 1)
+    {
       Serial.print("Mirobot: ");
       Serial.println(strMsg);
     }
   }
-  else{
-    String rs485Cmd ="@" + address + gcode;
-	  while(Serial1.read() >= 0){};
-    Serial1.println(rs485Cmd);
-    Serial1.flush();
-    delay(50);
-    if(serialMonitoringMode == 1){
+  else
+  {
+    String rs485Cmd = "@" + address + gcode;
+    while (Serial2.read() >= 0)
+    {
+    };
+    Serial2.println(rs485Cmd);
+    Serial2.flush();
+    delay(100);
+    if (serialMonitoringMode == 1)
+    {
       Serial.print("RS485 message: ");
       Serial.println(rs485Cmd);
     }
     unsigned long rtime = millis();
     strMsg = "";
-    while(strMsg.indexOf("ok")<0&&strMsg.indexOf("State")<0&&check&&strMsg.length()!=2){
-      //如果串口中有数据则全部读取
-      //if(Serial1.available()) strMsg = Serial1.readString();
-     // if(Serial1.available()) strMsg = Serial1.readStringUntil('/r/n');
-     while(Serial1.available()){
-        char c = Serial1.read();
-       // if (c == '\n') {
-         // Serial.println(serialBuffer);
-       // }else if (c != '\r') {
-        if (c != '\r') {
-          strMsg += c;  // เก็บตัวอักษรใน buffer
+    while (strMsg.indexOf("ok") < 0 && strMsg.indexOf("State") < 0 && check && strMsg.length() != 2)
+    {
+      // If there is data in the serial port, read all of it
+      // if(Serial2.available()) strMsg = Serial2.readString();
+      // if(Serial2.available()) strMsg = Serial2.readStringUntil('/r/n');
+      while (Serial2.available())
+      {
+        char c = Serial2.read();
+        // if (c == '\n') {
+        // Serial.println(serialBuffer);
+        // }else if (c != '\r') {
+        if (c != '\r')
+        {
+          strMsg += c; // Store the character in the buffer
         }
       }
-      //如果等待结束符超时则跳出
-      if((millis()-rtime)>=(mirobotCmdTimeout)){
+      // If waiting for the end character times out, break out
+      if ((millis() - rtime) >= (mirobotCmdTimeout))
+      {
         strMsg += "---Timeout---";
         break;
       }
-      //delay(100);
+      // delay(100);
     }
-    if(serialMonitoringMode == 1){
+    if (serialMonitoringMode == 1)
+    {
       Serial.print("Received: ");
       Serial.println(strMsg);
     }
   }
 }
 
-int UART_Mirobot::getStatus(){
-  sendMsg("o103");
+int UART_Mirobot::getStatus()
+{
+  sendMsg("O103");
   int Status;
-  if(strMsg.indexOf("Offline")>=0 || strMsg.indexOf("0")>=0) Status = Offline;
-  else if(strMsg.indexOf("Idle")>=0 || strMsg.indexOf("1")>=0) Status = Idle;
-  else if(strMsg.indexOf("Alarm")>=0 || strMsg.indexOf("2")>=0) Status = Alarm;
-  else if(strMsg.indexOf("Homing") >= 0 || strMsg.indexOf("3") >= 0) Status = Homing;
-  else if(strMsg.indexOf("Run")>=0 || strMsg.indexOf("5")>=0) Status = Run;
-  else if(strMsg.indexOf("Busy")>=0 || strMsg.indexOf("4")>=0) Status = Busy;
-  else Status = Offline;
+  if (strMsg.indexOf("Offline") >= 0 || strMsg.indexOf("0") >= 0)
+    Status = Offline;
+  else if (strMsg.indexOf("Idle") >= 0 || strMsg.indexOf("1") >= 0)
+    Status = Idle;
+  else if (strMsg.indexOf("Alarm") >= 0 || strMsg.indexOf("2") >= 0)
+    Status = Alarm;
+  else if (strMsg.indexOf("Homing") >= 0 || strMsg.indexOf("3") >= 0)
+    Status = Homing;
+  else if (strMsg.indexOf("Run") >= 0 || strMsg.indexOf("5") >= 0)
+    Status = Run;
+  else if (strMsg.indexOf("Busy") >= 0 || strMsg.indexOf("4") >= 0)
+    Status = Busy;
+  else
+    Status = Offline;
   return Status;
 }
-int UART_Mirobot::getResponse(char delimiter, String values[], int maxValues) {
-  String msg = strMsg;
- // msg.trim(); // ตัด \r\n และช่องว่าง
 
-  // ตัด "ok" ด้านท้ายถ้ามี
-  if (msg.endsWith("ok")) {
+int UART_Mirobot::getResponse(char delimiter, String values[], int maxValues)
+{
+  String msg = strMsg;
+  // msg.trim(); // Trim \r\n and spaces
+
+  // Remove "ok" from the end if it exists
+  if (msg.endsWith("ok"))
+  {
     msg.remove(msg.length() - 2);
   }
 
@@ -232,34 +333,41 @@ int UART_Mirobot::getResponse(char delimiter, String values[], int maxValues) {
   int end = msg.indexOf(delimiter);
   int count = 0;
 
-  while (end != -1 && count < maxValues) {
+  while (end != -1 && count < maxValues)
+  {
     values[count++] = msg.substring(start, end);
     start = end + 1;
     end = msg.indexOf(delimiter, start);
   }
 
-  // ค่าสุดท้าย
-  if (start < msg.length() && count < maxValues) {
+  // The last value
+  if (start < msg.length() && count < maxValues)
+  {
     values[count++] = msg.substring(start);
   }
 
-  return count; // คืนจำนวนค่า
+  return count; // Return the number of values
 }
 
-void UART_Mirobot::runFile(String fileName){
-  while(getStatus() != Idle && getStatus() != Alarm);
+void UART_Mirobot::runFile(String fileName)
+{
+  while (getStatus() != Idle && getStatus() != Alarm)
+    ;
   String strCmd = "o111" + fileName;
   sendMsg(strCmd);
   delay(1000);
-  while(getStatus() != Idle);
+  while (getStatus() != Idle)
+    ;
 }
 
 /*****************************************/
 
-void RS485_Mirobot::rs485Init(int add, long baudRate,int RX_PIN,int TX_PIN){
+void RS485_Mirobot::rs485Init(int add, long baudRate, int RX_PIN, int TX_PIN)
+{
   commMode = 1;
   address = String(add);
-  if(address.length()<2) address = "0" + address;
+  if (address.length() < 2)
+    address = "0" + address;
   pinMode(RX_PIN, INPUT_PULLUP);
-  Serial1.begin(baudRate, SERIAL_8N1, RX_PIN, TX_PIN);
+  Serial2.begin(baudRate, SERIAL_8N1, RX_PIN, TX_PIN);
 }
--- libraries/Mirobot/Mirobot.h
+++ libraries/Mirobot/Mirobot.h
@@ -25,53 +25,52 @@
 #define Run 5
 #define Busy 4
 
-class UART_Mirobot{
-  public:
-    UART_Mirobot();
-    void serialInit(int serialPort = 0);    //设置与Mirobot通信的串口（0/Serial; 1/Serial1; 2/Serial2; 3/Serial3)
-    void setSerialMonitoring(bool mode);    //设置开启串口监视，通过Arduino串口监视器查看
-    void setCmdTimeout(long cmdTimeout);   //设置每条指令发送后超时等待的时间（单位：毫秒ms）
-    void homing(int homingMode = 0);    //控制机械臂归零（0/$H：快速归零；1/$H1：1轴归零；2/$H2：2轴归零；3/$H3：3轴归零；4/$H4：4轴归零；
-                                        //5/$H5：5轴归零；6/$H6：6轴归零（需对应硬件支持）；7/$H7：滑轨归零；
-    void zero();    //机械臂初始位置
-    void setMoveSpeed(int velocity);    //设置机械臂运动速度
-    void setJumpPara(int height);   //设置JUMP门型轨迹抬起高度
-    void timedPause(float seconds);   //暂停等待（单位：秒s）
-    
-    void moveJoints(float j1, float j2, float j3, float j4, float j5, float j6, bool motionMode = ABS);   //角度模式运动
-    void movePose(float x, float y, float z, float rx, float ry, float rz, bool motionMode = ABS, int pathMode = MOVJ);   //坐标模式运动
-    void moveArc(float x, float y, float z, float r, bool motionMode = ABS, int pathMode = CW);   //圆弧插补运动
-    
-    void gripper(int gripperStatus);    //舵机夹爪状态
-    void softGripper(int softGripperStatus);    //三指柔爪状态
-    void pump(int pumpStatus);    //气泵状态
-    void pwmWrite(int pwm);   //pwm（控制器黄色接口）输出，范围：0~1000
+class UART_Mirobot
+{
+public:
+  UART_Mirobot();
+  void serialInit(int serialPort = 0); // Set the serial port for communication with Mirobot (0/Serial; 1/Serial1; 2/Serial2; 3/Serial3)
+  void setSerialMonitoring(bool mode); // Set to enable serial monitoring, view through Arduino serial monitor
+  void setCmdTimeout(long cmdTimeout); // Set the timeout waiting time after each instruction is sent (unit: milliseconds ms)
+  void homing(int homingMode = 0);     // Control the robotic arm homing (0/$H: quick homing; 1/$H1: axis 1 homing; 2/$H2: axis 2 homing; 3/$H3: axis 3 homing; 4/$H4: axis 4 homing;
+                                   // 5/$H5: axis 5 homing; 6/$H6: axis 6 homing (requires corresponding hardware support); 7/$H7: rail homing;
+  void zero();                     // Robotic arm initial position
+  void setMoveSpeed(int velocity); // Set the robotic arm movement speed
+  void setJumpPara(int height);    // Set the JUMP gate-shaped trajectory lift height
+  void timedPause(float seconds);  // Pause/wait (unit: seconds s)
 
-    void moveConveyor(float d, bool motionMode = ABS);    //传送带运动控制
-    void moveRail(float d, bool motionMode = ABS);    //滑轨运动控制
+  void moveJoints(float j1, float j2, float j3, float j4, float j5, float j6, bool motionMode = ABS);                 // Angle mode movement
+  void movePose(float x, float y, float z, float rx, float ry, float rz, bool motionMode = ABS, int pathMode = MOVJ); // Coordinate mode movement
+  void moveArc(float x, float y, float z, float r, bool motionMode = ABS, int pathMode = CW);                         // Arc interpolation movement
 
-    int getStatus();  //获取机械臂工作状态，0/1/2/3/4/5： 0机械臂离线 1正常 2锁定 3复位中 4设备忙 5运行中 
-    int getResponse(char delimiter, String values[], int maxValues);
-    void runFile(String fileName);  //运行控制器中存储的Gcode文件
-    void sendMsg(String gcode, bool check = ON);
+  void gripper(int gripperStatus);         // Servo gripper status
+  void softGripper(int softGripperStatus); // Three-finger soft gripper status
+  void pump(int pumpStatus);               // Air pump status
+  void pwmWrite(int pwm);                  // pwm (controller yellow interface) output, range: 0~1000
 
-  protected:
-    bool commMode;
-    int mirobotSerialPort = 0;
-    bool serialMonitoringMode = 1;
-    int mirobotVelocity = 2000;
-    long mirobotCmdTimeout = 10000L;
-    void mirobotReceived();
-    String strMsg = "";
-    String address;
+  void moveConveyor(float d, bool motionMode = ABS); // Conveyor belt movement control
+  void moveRail(float d, bool motionMode = ABS);     // Slide rail movement control
+
+  int getStatus(); // Get the robotic arm working status, 0/1/2/3/4/5: 0 robotic arm offline 1 normal 2 locked 3 resetting 4 device busy 5 running
+  int getResponse(char delimiter, String values[], int maxValues);
+  void runFile(String fileName); // Run the Gcode file stored in the controller
+  void sendMsg(String gcode, bool check = ON);
+
+protected:
+  bool commMode;
+  int mirobotSerialPort = 0;
+  bool serialMonitoringMode = 1;
+  int mirobotVelocity = 2000;
+  long mirobotCmdTimeout = 10000L;
+  void mirobotReceived();
+  String strMsg = "";
+  String address;
 };
 
-class RS485_Mirobot: public UART_Mirobot{
-  public:
-    void rs485Init(int add = 1, long baudRate = 115200, int RX_PIN = 4, int TX_PIN = 5);
-
+class RS485_Mirobot : public UART_Mirobot
+{
+public:
+  void rs485Init(int add = 1, long baudRate = 38400, int RX_PIN = 4, int TX_PIN = 5);
 };
-
-
 
 #endif
