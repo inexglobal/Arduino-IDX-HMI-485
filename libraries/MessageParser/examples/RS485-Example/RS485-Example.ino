3112a805c7da10f6ac88763f8260b6c393dc43cf
inexglobal <chaiwat@inex.co.th>
2025-11-22 20:36:15 +0700

    Update RS485-Example.ino

--- libraries/MessageParser/examples/RS485-Example/RS485-Example.ino
+++ libraries/MessageParser/examples/RS485-Example/RS485-Example.ino
@@ -1,32 +1,37 @@
 #include <MessageParser.h>
-
+// Define 485 communication pins
+#define RS485_RX_PIN 43
+#define RS485_TX_PIN 44
+// Redefine serial port name
+#define RS485 Serial1
+String strMsg = "";
 MessageParser parser;
 
 void setup() {
   Serial.begin(115200);
-  delay(500);
-
-  String msg1 = "@03o131=3319,33";
-  String msg2 = "@01o555?";
-
-  ParsedMessage p1 = parser.parse(msg1);
-  ParsedMessage p2 = parser.parse(msg2);
-
-  Serial.println("Message 1:");
-  Serial.println("Header: " + p1.header);
-  Serial.println("Command: " + p1.command);
-  for (int i = 0; i < p1.valueCount; i++) {
-    Serial.print("Value ");
-    Serial.print(i);
-    Serial.print(": ");
-    Serial.println(p1.values[i]);
+  // Initialize 485 device
+  pinMode(RS485_RX_PIN, INPUT_PULLUP);
+  RS485.begin(115200, SERIAL_8N1, RS485_RX_PIN, RS485_TX_PIN);
+  while (!RS485) {
+    delay(10);  // Wait for initialization to succeed
   }
-
-  Serial.println("\nMessage 2:");
-  Serial.println("Header: " + p2.header);
-  Serial.println("Command: " + p2.command);
-  Serial.print("Is Query: ");
-  Serial.println(p2.isQuery ? "true" : "false");
 }
-
-void loop() {}
+void loop() {
+  if (RS485.available()) {
+    strMsg = RS485.readString();
+    RS485.flush();
+    ParsedMessage p1 = parser.parse(strMsg);
+    Serial.println("Message 1:");
+    Serial.println("Header: " + p1.header);
+    Serial.println("Command: " + p1.command);
+    Serial.print("Is Query: ");
+    Serial.println(p1.isQuery ? "true" : "false");
+    for (int i = 0; i < p1.valueCount; i++) {
+      Serial.print("Value ");
+      Serial.print(i);
+      Serial.print(": ");
+      Serial.println(p1.values[i]);
+    }
+    RS485.print(strMsg);
+  }
+}
