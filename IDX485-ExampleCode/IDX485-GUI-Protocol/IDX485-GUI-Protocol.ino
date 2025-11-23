adb1013776079641aa4fe5d23c4366fe3e1a5357
inexglobal <chaiwat@inex.co.th>
2025-11-23 14:00:42 +0700

    Update IDX485-GUI-Protocol.ino

--- IDX485-ExampleCode/IDX485-GUI-Protocol/IDX485-GUI-Protocol.ino
+++ IDX485-ExampleCode/IDX485-GUI-Protocol/IDX485-GUI-Protocol.ino
@@ -5,9 +5,9 @@ int32_t _count = 0;
 int32_t _flag_reset = 0;
 int32_t _flag_mode = 0;
 void setup() {
-  Serial.begin(115200);          //Initialize the serial port, the baud rate is 115200
-  HMI485.rs485Init(99, 115200);  //Initial setting, default address 01
-  HMI485.setSerialMonitoring(1); //Monitor setting, default 1:Open, 0:Off
+  Serial.begin(115200);           //Initialize the serial port, the baud rate is 115200
+  HMI485.rs485Init(99, 115200);   //Initial setting, default address 01
+  HMI485.setSerialMonitoring(1);  //Monitor setting, default 1:Open, 0:Off
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
 }
@@ -20,9 +20,11 @@ void loop() {
   for (int i = 0; i < count; i++) {
     Serial.println(values[i]);
   }
-  _flag_mode = values[0].toInt();
-  _count = values[1].toInt() + 1;
-  _flag_reset = values[2].toInt();
-  digitalWrite(8,(_flag_mode ? 0:1 ));
-  digitalWrite(9,(_flag_reset ? 0:1 ));
+  if (count > 0) {
+    _flag_mode = values[0].toInt();
+    _count = values[1].toInt() + 1;
+    _flag_reset = values[2].toInt();
+    digitalWrite(8, (_flag_mode ? 0 : 1));
+    digitalWrite(9, (_flag_reset ? 0 : 1));
+  }
 }
